3d5cd5d5b9cbbef3e3e18290994bc0d4ad9e9ad6
Alexander Interrante-Grant <a.interrantegrant@gmail.com>
2024-01-26 12:37:23 -0500

    reimplement memory stuff as State Values (WIP)

--- smallworld/state.py
+++ smallworld/state.py
@@ -203,46 +203,40 @@ class RegisterAlias(Register):
 
 
 class Memory(Value):
-    """A memory region value.
+    """A memory region base class.
 
     Arguments:
         address (int): The address of this memory region.
         size (int): The size (in bytes) of this memory region.
     """
 
-    def __init__(self, address: int, size: int):
+    def __init__(self, address: int, size: int, byteorder="little"):
         self.address = address
         self.size = size
-        self.value: typing.Optional[bytes] = None
-
-    def get(self) -> typing.Optional[bytes]:
-        return self.value
-
-    def set(self, value: bytes) -> None:
-        self.value = value
+        self.byteorder = byteorder
 
     def initialize(
         self, initializer: initializer.Initializer, override: bool = False
     ) -> None:
-        if self.value is not None and not override:
+        if self.get() is not None and not override:
             logger.debug(f"skipping initialization for {self} (already initialized)")
             return
 
         self.set(initializer.generate(self.size))
 
     def load(self, executor: executor.Executor, override: bool = True) -> None:
-        if self.value is not None and not override:
+        if self.get() is not None and not override:
             logger.debug(f"skipping load for {self} (already loaded)")
             return
 
-        self.value = executor.read_memory(self.address, self.size)
+        self.set(executor.read_memory(self.address, self.size))
 
     def apply(self, executor: executor.Executor) -> None:
-        executor.write_memory(self.address, self.value)
+        executor.write_memory(self.address, self.get())
 
     def __repr__(self) -> str:
-        if self.value:
-            value = self.value.decode("utf-8", errors="replace")
+        if self.get():
+            value = self.get().decode("utf-8", errors="replace")
             value = textwrap.shorten(value, width=32)
 
             rep = f'{self.address:x}="{value}"[{self.size}]'
@@ -251,9 +245,127 @@ class Memory(Value):
 
         return f"Memory({rep})"
 
+    def to_bytes(self, value, size: typing.Optional[int] = None):
+        """Convert a given value to bytes.
+
+        Arguments:
+            value: Object to be converted.
+            size (int): Optional size.
+        """
+
+        if type(value) in (bytes, bytearray):
+            return value
+        elif type(value) is int:
+            if size is None:
+                raise ValueError("need a size to convert int to bytes")
+            return value.to_bytes(size, byteorder=self.byteorder)
+        else:
+            raise NotImplementedError(f"unsupported type: {type(value)}")
+
+
+class Stack(Memory):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.memory = []
+        self.used = 0
+
+    def get(self) -> typing.Optional[bytes]:
+        value = bytearray()
+
+        for v, s in self.memory:
+            value += self.to_bytes(v, s)
+
+        return value
+
+    def set(self, value: bytes) -> None:
+        raise NotImplementedError()
+
+    def push(self, value, size=None):
+        allocation = len(self.to_bytes(value, size))
+
+        if self.used + allocation > self.size:
+            raise ValueError(f"{value} (size: {allocation}) is too large for {self}")
+
+        self.memory.append((value, size))
+        self.used += allocation
+
+
+class Heap(Memory):
+    @abc.abstractmethod
+    def malloc(self, value, size: typing.Optional[int] = None) -> int:
+        """Place a value on the heap.
+
+        Arguments:
+            value: Object to be allocated.
+            size (int): Optional size.
+
+        Returns:
+            The address of the value allocated.
+        """
+
+        return 0
+
+    @abc.abstractmethod
+    def free(self, address: int) -> None:
+        """Free a value from the heap.
+
+        Arguments:
+            address (int): The address of the object to free.
+        """
+
+        pass
+
+
+class BumpAllocator(Heap):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.memory = []
+        self.used = 0
+
+    def get(self) -> typing.Optional[bytes]:
+        value = bytearray()
+
+        for v, s in self.memory:
+            value += self.to_bytes(v, s)
+
+        return value
+
+    def set(self, value: bytes) -> None:
+        raise NotImplementedError()
+
+    def malloc(self, value, size: typing.Optional[int] = None) -> int:
+        allocation = len(self.to_bytes(value, size))
+
+        if self.used + allocation > self.size:
+            raise ValueError(f"{value} (size: {allocation}) is too large for {self}")
+
+        address = self.address + self.used
+        self.memory.append((value, size))
+        self.used += allocation
+
+        return address
+
+    def free(self, address: int) -> None:
+        raise NotImplementedError()
+
 
 class State(Value):
     """A collection of state values, loaded and applied as a group."""
+
+    def map(self, value: Value, name: typing.Optional[str] = None) -> None:
+        """Map a given Value into this state object.
+
+        Arguments:
+            value (Value): Value to map.
+            name (str): Optional attribute name - defaults to the class name.
+        """
+
+        if name is None:
+            name = value.__class__.__name__.lower()
+
+        setattr(self, name, value)
 
     @property
     def values(self) -> typing.Dict[str, Value]:
@@ -308,15 +420,11 @@ class State(Value):
             state.initialize(initializer, override=override)
 
     def load(self, executor: executor.Executor, override: bool = True) -> None:
-        #        logger.info(f"loading {self} from {executor}")
-
         for name, state in self.values.items():
             logger.debug(f"loading {name} from {executor}")
             state.load(executor, override=override)
 
     def apply(self, executor: executor.Executor) -> None:
-        #        logger.info(f"applying {self} to {executor}")
-
         for name, state in self.values.items():
             logger.debug(f"applying {name}:{state} to {executor}")
             state.apply(executor)
