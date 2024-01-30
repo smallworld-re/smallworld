241f64d0261f2d54aeb57d78a01046713a45f132
Alexander Interrante-Grant <a.interrantegrant@gmail.com>
2024-01-30 16:18:43 -0500

    stack dream -> reality

--- tests/stack_0.py
+++ tests/stack_0.py
@@ -1,29 +1,19 @@
-# NOTE: you have to get rid of hints file first before
-# hinting gets set up. that is, right here.
-import os
-
-hintsfile = "hints.jsonl"
-if os.path.exists(hintsfile):
-    os.remove(hintsfile)
-
 import logging
 
-import smallworld.smallworld as sw
-from smallworld import utils
+from smallworld import cpus, executor, initializer, utils
 
 utils.setup_logging(level=logging.INFO)
 utils.setup_hinting(verbose=True, stream=True, file="hints.jsonl")
 
-# createa a small world
-conf = sw.X86_64()
-smw = sw.Smallworld(config=conf)
+# load code
+code = executor.Code.from_filepath("stack.bin", base=0x1000, entry=0x1000)
 
-# NOTE: code is of type bytes
-code = open("stack.bin", "rb").read()
+# create a cpu state
+cpu = cpus.AMD64CPUState()
 
-# map the code into memory at this address
-smw.map_code(base=0x1000, entry=0x1000, code=code)
+# initialize it
+zero = initializer.ZeroInitializer()
+cpu.initialize(zero)
 
-# analyze the code given that entry point
-# output: this will log hints both to stdout and to the file "hints.jsonl"
-smw.analyze()
+# run an analysis
+utils.analyze(code, cpu)
--- tests/stack_1.py
+++ tests/stack_1.py
@@ -1,48 +1,37 @@
 import logging
 
-import smallworld.smallworld as sw
-from smallworld import utils
+from smallworld import cpus, executor, initializer, state, utils
 
 utils.setup_logging(level=logging.INFO)
 utils.setup_hinting(verbose=True, stream=True, file="hints.jsonl")
 
-# createa a small world
-conf = sw.X86_64()
-smw = sw.Smallworld(config=conf)
+# load code
+code = executor.Code.from_filepath("stack.bin", base=0x1000, entry=0x1000)
 
-# NOTE: code is of type bytes
-code = open("stack.bin", "rb").read()
+# create a cpu state
+cpu = cpus.AMD64CPUState()
 
-# map the code into memory at this address
-smw.map_code(base=0x1000, entry=0x1000, code=code)
+# initialize it
+zero = initializer.ZeroInitializer()
+cpu.initialize(zero)
 
-# analysis from stack_0.py told us
-# that rdi & rdx & r8 are uninitialized.
-# Also program fails bc there's no stack (invalid memory read of [rsp+8]
-# i.e. memory pointed to by rsp doesnt exist
-#
-# So we turn off the analysis and add the following
+# initialize some values
+cpu.rdi.set(0x11111111)
+cpu.rdx.set(0x22222222)
+cpu.r8.set(0x33333333)
 
-# create initial values for
-# rdi, rdx, r8 which are are args 1, 3, and 5 to this fn
-smw.cpu.rdi.set(0x11111111)
-smw.cpu.rdx.set(0x22222222)
-smw.cpu.r8.set(0x33333333)
-
-# and create stack in order to set final arg used
-stack = sw.Stack(base_addr=0x2000, size=0x1000, config=conf)
-
-# this is the 7th arg to `stack.s`
+# create a stack and push a value
+stack = state.Stack(address=0x2000, size=0x1000)
 stack.push(value=0x44444444, size=8)
 
 # map the stack into memory
-smw.map_region(stack)
+cpu.map(stack)
 
-# set rsp to top of stack
-smw.cpu.rsp.set(stack.base_addr)
+# set the stack pointer
+cpu.rsp.set(stack.address)
 
-# now we can do a single micro-execution without error
-final_state = smw.emulate(num_instructions=3)
+# emulate
+final = utils.emulate(code, cpu)
 
-# read out the answer in rax
-print(final_state.rax)
+# read out the final state
+print(final.rax)
