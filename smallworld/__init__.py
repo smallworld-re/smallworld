ee1d6711279ec89acf7966abb3d03eaccc09c9e7
Alexander Interrante-Grant <a.interrantegrant@gmail.com>
2024-02-02 08:34:47 -0500

    fix package build (remove depenencies for `egg_info`)

--- smallworld/__init__.py
+++ smallworld/__init__.py
@@ -1,9 +1,3 @@
-__title__ = "SmallWorld"
-__author__ = "MIT Lincoln Laboratory"
-__description__ = "An emulation state tracking library and tool"
-
-__version__ = "0.1.0.dev"
-
 from . import analyses, cpus, emulators, exceptions, hinting, initializers, state
 from .emulators import Code
 from .utils import analyze, emulate, setup_hinting, setup_logging
