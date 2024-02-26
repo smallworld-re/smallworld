e9a8ac48a531f4978896bbe51dfef4a4c3570bf6
Alex Interrante-Grant <a.interrantegrant@gmail.com>
2024-02-26 11:49:48 -0500

    chore: added changelog (#31)

--- smallworld/__init__.py
+++ smallworld/__init__.py
@@ -4,6 +4,7 @@ metadata = __metadata.metadata("smallworld")
 
 __title__ = metadata["name"]
 __description__ = metadata["Summary"]
+__author__ = metadata["Author"]
 __version__ = metadata["version"]
 
 from . import analyses, cpus, emulators, exceptions, hinting, initializers, state
