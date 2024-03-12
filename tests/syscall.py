61ef1dab0834c4b2f4d8713b150a9a0253209e20
Alex Heinricher <aheinricher@gmail.com>
2024-03-12 09:20:26 -0400

    Added test harness for syscall
    This test currently fails, which is expected.
    I'm not sure if the exact exception is expected.

---
+++ tests/syscall.py
@@ -0,0 +1,39 @@
+import logging
+
+import smallworld
+
+smallworld.setup_logging(level=logging.INFO)
+smallworld.setup_hinting(verbose=True, stream=True, file=None)
+
+# create a state object
+state = smallworld.cpus.AMD64CPUState()
+
+# load and map code into the state and set ip
+code = smallworld.state.Code.from_filepath("syscall.bin", base=0x1000, entry=0x1000)
+state.map(code)
+state.rip.set(0x1000)
+
+# create a stack and push a value
+data = b"Hello, world!\n\0"
+stack = smallworld.state.Stack(address=0x2000, size=0x1000)
+stack.push(value=data, size=len(data))
+
+# map the stack into memory
+state.map(stack)
+
+# set the stack pointer
+state.rsp.set(stack.address)
+
+# Initialize call to write():
+# - edi: File descriptor 1 (stdout)
+# - rsi: Buffer containing output
+# - rdx: Size of output buffer
+state.edi.set(0x1)
+state.rsi.set(stack.address)
+state.rdx.set(len(data) - 1)
+
+# emulate
+final = smallworld.emulate(state)
+
+# read out the final state
+print(final.rax)
