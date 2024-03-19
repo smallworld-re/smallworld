bd872e7e98c84521a84d4b2fc52ffcb0ce9c3f8c
Tim Leek <tleek@ll.mit.edu>
2024-03-19 15:50:24 -0400

    maybe lint

--- tests/lb_calloc.py
+++ tests/lb_calloc.py
@@ -1,7 +1,10 @@
 import logging
+import random
 import sys
 
-'''
+import smallworld
+
+"""
 Harness for weird logic in LB that takes output of C's `rand()` and uses it
 to compute 1st argument to `calloc()`.
 
@@ -18,9 +21,7 @@ In this harness, we micro-execute a number of times determined by argv[1].  For
 each, rax is initialized with a plausible return value from `rand()`.  The
 output in `rdi` is displayed.
 
-'''
-
-import smallworld
+"""
 
 smallworld.setup_logging(level=logging.INFO)
 smallworld.setup_hinting(verbose=True, stream=True, file=None)
@@ -33,21 +34,17 @@ code = smallworld.state.Code.from_filepath("lb_calloc.bin", base=0x1000, entry=0
 state.map(code)
 state.rip.set(0x1000)
 
-
-import random
-RAND_MAX=0x7fffffff
+RAND_MAX = 0x7FFFFFFF
 
 for i in range(int(sys.argv[1])):
-
-    randv = random.randint(0,RAND_MAX)
+    randv = random.randint(0, RAND_MAX)
     print(f"randv = {randv}")
 
     state.eax.set(randv)
     print(state.eax.get())
-    
+
     # now we can do a single micro-execution without error
     final_state = smallworld.emulate(state)
-    
+
     # read the result
     print(final_state.rdi)
-    
