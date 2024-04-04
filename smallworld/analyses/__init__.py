c3505271f5a499bed1798c72692c609d99ff5673
bigandian <bigandian@users.noreply.github.com>
2024-04-04 15:40:51 -0400

    feat: code reachability analysis (#75)

--- smallworld/analyses/__init__.py
+++ smallworld/analyses/__init__.py
@@ -1,5 +1,6 @@
 from .analysis import Analysis, Filter
 from .code_coverage import CodeCoverage
+from .code_reachable import CodeReachable
 from .control_flow_tracer import ControlFlowTracer
 from .input_colorizer import InputColorizerAnalysis
 
@@ -9,4 +10,5 @@ __all__ = [
     "InputColorizerAnalysis",
     "ControlFlowTracer",
     "CodeCoverage",
+    "CodeReachable",
 ]
