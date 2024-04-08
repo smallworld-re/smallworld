1d47bdd32a88dcb6021c2d3b0e8748e25b20ae75
tleek <trleek@gmail.com>
2024-04-08 14:53:32 -0400

    hashing thigns
    Co-authored-by: Alex Interrante-Grant <a.interrantegrant@gmail.com>

--- smallworld/instructions.py
+++ smallworld/instructions.py
@@ -51,8 +51,8 @@ class RegisterOperand(Operand):
     def __eq__(self, other):
         return self.__repr__() == other.__repr__()
 
-    def __hash__(self):
-        return hash(self.__repr__())
+    def __hash__(self) -> int:
+        return hash((self.name, self.size))
 
     def concretize(self, emulator: emulators.Emulator) -> int:
         return emulator.read_register(self.name)
