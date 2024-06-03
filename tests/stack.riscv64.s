11fdef7d2773decbc1ed35bb7e768810d3c602e5
Alex Heinricher <aheinricher@gmail.com>
2024-06-03 13:23:46 -0400

    Added multi-architecture test cases
    The test case build system now uses gasm to support
    the following target platforms:
    
    - aarch64
    - armv5T (I think...)
    - armv7
    - mips32r2 (WLOG: big-endian)
    - mips64r2 (WLOG: big-endian)
    - powerpc
    - powerpc64
    - riscv64
    - sparc64
    
    The makefile will build for all target platforms by default.
    It will test to see if the appropriate tools are available,
    and produce an error if they are not.
    
    To bypass this feature and only build x86-64 tests with nasm,
    run `make nasm`
    
    There are now cross-arch examples of the following tests:
    
    - recursion
    - square
    - stack
    - strlen

---
+++ tests/stack.riscv64.s
@@ -0,0 +1,7 @@
+    .text
+multiargs:
+    lw a5,0(sp)
+    addw a0,a0,a2
+    addw a0,a0,a4
+    addw a0,a0,a6
+    addw a0,a0,a5 
---
+++ tests/strlen.mips.s
@@ -0,0 +1,15 @@
+    .text
+strlen:
+    li      $2,0
+$L2:
+    lb      $3,($4)
+    beq     $3,$0,$L3
+    nop
+    addiu   $2,$2,1
+    addiu   $4,$4,1
+    b       .L2
+    nop
+$L3:
+    jr      $31
+    nop
+    
