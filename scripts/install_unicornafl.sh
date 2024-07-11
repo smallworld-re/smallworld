f675c74ec41bbabe630749724279bb70869f3aaa
Alex Heinricher <aheinricher@gmail.com>
2024-07-11 13:35:53 -0400

    Removed unicornafl install script.
    This is better served by documenting the process.
    The instructions here were not intended as the
    production installation solution,
    only a repeatable solution for CI/CD.

--- scripts/install_unicornafl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-scriptdir=`dirname "$(realpath -s $0)"`
-projdir=`dirname "$scriptdir"`
-depsdir="$projdir/tests/dependencies"
-
-sudo apt-get update || exit 1
-sudo apt-get install -y `find "$depsdir" -name '*.deb'`|| exit 1
-python -m pip install `find "$depsdir" -name '*.whl'` || exit 1
