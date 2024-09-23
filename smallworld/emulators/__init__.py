8de4ffa21f12021d91d9784db3656d0759bdc620
Alexander Interrante-Grant <a.interrantegrant@gmail.com>
2024-09-23 14:07:58 -0400

    update `square.amd64.py`

--- smallworld/emulators/__init__.py
+++ smallworld/emulators/__init__.py
@@ -1,4 +1,6 @@
 from .emulator import *  # noqa: F401, F403
 from .emulator import __all__ as __emulator__
+from .unicorn import *  # noqa: F401, F403
+from .unicorn import __all__ as __unicorn__
 
-__all__ = __emulator__
+__all__ = __emulator__ + __unicorn__
--- tests/square/square.amd64.py
+++ tests/square/square.amd64.py
@@ -1,12 +1,17 @@
 import sys
+
 import smallworld
 
-machine = smallworld.Machine()
-code = smallworld.state.Code.from_filepath("square.amd64.bin", base=0x1000, entry=0x1000)
+machine = smallworld.state.Machine()
+code = smallworld.state.memory.code.Executable.from_filepath(
+    "square.amd64.bin", address=0x1000
+)
 machine.add(code)
 
-platform = smallworld.platform.Platform(smallworld.platform.Architecture.X86_64, smallworld.platform.Byteorder.LITTLE, smallworld.platform.ABI.SYSTEMV)
-cpu = smallworld.state.CPU.for_platform(platform)
+platform = smallworld.platforms.Platform(
+    smallworld.platforms.Architecture.X86_64, smallworld.platforms.Byteorder.LITTLE
+)
+cpu = smallworld.state.cpus.CPU.for_platform(platform)
 cpu.rip.set(0x1000)
 cpu.edi.set(int(sys.argv[-1]))
 machine.add(cpu)
@@ -14,4 +19,4 @@ machine.add(cpu)
 emulator = smallworld.emulators.UnicornEmulator(platform)
 final_machine = machine.emulate(emulator)
 
-print(final_machine.cpu.eax.get())
+print(final_machine.cpu.eax)
