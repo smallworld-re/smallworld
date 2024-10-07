5646126a73c75b6a83133a0cc40b910e639d69c8
andy <andy@bigandian.com>
2024-10-07 13:30:43 -0400

    RawMemory added

--- smallworld/state/memory/code.py
+++ smallworld/state/memory/code.py
@@ -4,7 +4,7 @@ import typing
 from ... import state,emulators, exceptions
 from . import memory
 
-class Executable(memory.Memory):
+class Executable(memory.RawMemory):
     """An execuable piece of code."""
 
     entrypoint: typing.Optional[int] = None
@@ -14,59 +14,6 @@ class Executable(memory.Memory):
         This is not used by Emulators - but is available for reference from
         file parsing, if supported.
     """
-
-    @classmethod
-    def from_bytes(cls, bytes: bytes, address: int):
-        """Load an executable from a byte array.
-
-        Arguments:
-            bytes: The bytes of the executable.
-            address: The address where this executable should be loaded.
-
-        Returns:
-            An Executable parsed from the given bytes.
-        """
-
-        executable = cls(address=address, size=len(bytes))
-        executable[0] = bytes
-
-        return executable
-
-    @classmethod
-    def from_file(cls, file: typing.BinaryIO, address: int):
-        """Load an executable from an open file-like object.
-
-        Arguments:
-            file: The open file-like object from which to read.
-            address: The address where this executable should be loaded.
-
-        Returns:
-            An Executable parsed from the given file-like object.
-        """
-
-        data, size = file.read(), file.tell()
-
-        executable = cls(address=address, size=size)
-        executable[0] = state.BytesValue(data, "code")
-
-        return executable
-
-    @classmethod
-    def from_filepath(cls, path: str, address: int):
-        """Load an executable from a file path.
-
-        Arguments:
-            path: The path to the file to load.
-            address: The address where this executable should be loaded.
-
-        Returns:
-            An Executable parsed from the given file path.
-        """
-
-        path = os.path.abspath(os.path.expanduser(path))
-
-        with open(path, "rb") as f:
-            return cls.from_file(file=f, address=address)
 
     @classmethod
     def from_elf(cls, file: typing.BinaryIO):
@@ -94,17 +41,6 @@ class Executable(memory.Memory):
             An Executable parsed from the given PE file-like object.
         """
         raise NotImplementedError("PE parsing not yet implemented")
-
-
-
-    def extract(self, emulator: emulators.Emulator) -> None:
-        try:
-            the_memory = emulator.read_memory(self.address, self.get_capacity())        
-            value = state.BytesValue(the_memory, "code")
-        except exceptions.SymbolicValueError:
-            value = state.EmptyValue(self.get_capacity(), None, "code")
-        self[0] = value
-
 
 
 __all__ = ["Executable"]
