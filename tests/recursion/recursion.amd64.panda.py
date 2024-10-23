5c2069451b484f9b414e8564c0c0fded41bc9b85
Heather Preslier <heather.preslier@ll.mit.edu>
2024-10-23 15:19:14 -0400

    Adding multi arch tests

---
+++ tests/recursion/recursion.amd64.panda.py
@@ -0,0 +1,53 @@
+import logging
+import sys
+
+import smallworld
+
+# Set up logging and hinting
+smallworld.logging.setup_logging(level=logging.INFO)
+smallworld.hinting.setup_hinting(stream=True, verbose=True)
+
+# Define the platform
+platform = smallworld.platforms.Platform(
+    smallworld.platforms.Architecture.X86_64, smallworld.platforms.Byteorder.LITTLE
+)
+
+# Create a machine
+machine = smallworld.state.Machine()
+
+# Create a CPU
+cpu = smallworld.state.cpus.CPU.for_platform(platform)
+machine.add(cpu)
+
+# Load and add code into the state
+code = smallworld.state.memory.code.Executable.from_filepath(
+    __file__.replace(".py", ".bin").replace(".angr", "").replace(".panda", ""),
+    address=0x1000,
+)
+machine.add(code)
+
+# Create a stack and add it to the state
+stack = smallworld.state.memory.stack.Stack.for_platform(platform, 0x2000, 0x4000)
+machine.add(stack)
+
+# Set the instruction pointer to the code entrypoint
+cpu.rip.set(code.address)
+
+# Initialize argument registers
+cpu.rdi.set(int(sys.argv[1]))
+
+# Push a return address onto the stack
+stack.push_integer(0xFFFFFFFF, 8, "fake return address")
+
+# Configure the stack pointer
+rsp = stack.get_pointer()
+cpu.rsp.set(rsp)
+
+# Emulate
+emulator = smallworld.emulators.PandaEmulator(platform)
+emulator.add_exit_point(cpu.rip.get() + code.get_capacity())
+final_machine = machine.emulate(emulator)
+
+# read out the final state
+cpu = final_machine.get_cpu()
+print(hex(cpu.eax.get()))
---
+++ tests/struct/struct.amd64.panda.py
@@ -0,0 +1,66 @@
+import ctypes
+import logging
+
+import smallworld
+
+smallworld.logging.setup_logging(level=logging.INFO)
+
+machine = smallworld.state.Machine()
+code = smallworld.state.memory.code.Executable.from_filepath(
+    __file__.replace(".py", ".bin").replace(".angr", "").replace(".panda", ""),
+    address=0x1000,
+)
+
+platform = smallworld.platforms.Platform(
+    smallworld.platforms.Architecture.X86_64, smallworld.platforms.Byteorder.LITTLE
+)
+cpu = smallworld.state.cpus.CPU.for_platform(platform)
+
+
+class StructNode(ctypes.LittleEndianStructure):
+    _pack_ = 8
+
+
+StructNode._fields_ = [
+    ("data", ctypes.c_int32),
+    ("padding_0x4", ctypes.c_int32),
+    ("next", smallworld.extern.ctypes.create_typed_pointer(StructNode)),
+    ("prev", smallworld.extern.ctypes.create_typed_pointer(StructNode)),
+    ("empty", ctypes.c_int32),
+]
+
+heap = smallworld.state.memory.heap.BumpAllocator(0x4000, 0x4000)
+
+node_a = StructNode()
+node_a.data = 2
+node_a.empty = 0
+node_a.prev = 0
+node_a_addr = heap.allocate(smallworld.state.Value.from_ctypes(node_a, "node a"))
+
+node_b = StructNode()
+node_b.data = 1
+node_b.empty = 1
+node_b.next = 0
+node_b_addr = heap.allocate(smallworld.state.Value.from_ctypes(node_b, "node b"))
+
+node_a.next = node_b_addr
+node_b.prev = node_a_addr
+
+machine.add(heap)
+machine.add(cpu)
+machine.add(code)
+
+cpu.rip.set(0x1000)
+cpu.rdi.set_content(node_a_addr)
+cpu.rsi.set_content(42)
+emulator = smallworld.emulators.PandaEmulator(platform)
+emulator.add_exit_point(cpu.rip.get() + 26)
+
+# final_machine = machine.emulate(emulator)
+# final_cpu = final_machine.get_cpu()
+
+*_, final_machine = machine.step(emulator)
+
+final_cpu = final_machine.get_cpu()
+print(f"curr = {hex(final_cpu.rdi.get())}")
+print(f"arg2 = {final_cpu.esi.get()}")
