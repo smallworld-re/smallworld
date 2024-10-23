ed45baebb98de147227b1d7b62ba68cc272d6beb
Alex Heinricher <aheinricher@gmail.com>
2024-10-23 14:19:07 -0400

    Fixed bug in arm elf tests

--- tests/elf/elf.armel.elf.s
+++ tests/elf/elf.armel.elf.s
@@ -1,6 +1,5 @@
-    .arch armv7-a
-	.fpu vfpv3-d16
-    .eabi_attribute 28, 1
+    .arch armv5
+	.fpu softvfp
 	.eabi_attribute 20, 1
 	.eabi_attribute 21, 1
 	.eabi_attribute 23, 3
@@ -8,7 +7,7 @@
 	.eabi_attribute 25, 1
 	.eabi_attribute 26, 2
 	.eabi_attribute 30, 6
-	.eabi_attribute 34, 1
+	.eabi_attribute 34, 0
 	.eabi_attribute 18, 4
     .text
     .align  2
