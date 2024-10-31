7d591f0765ce1ad35ee081287eed2159d9bbd147
Alex Heinricher <aheinricher@gmail.com>
2024-10-31 09:00:02 -0400

    Fixed strlen mips64 test

--- tests/strlen/strlen.mips64.s
+++ tests/strlen/strlen.mips64.s
@@ -1,12 +1,23 @@
     .text
+    .set    noreorder
+    .set    nomacro
+_start:
+    bal     main
+    nop                     # Delay slot
 strlen:
     li      $2,0
 .L2:
     lb      $3,($4)
     beq     $3,$0,.L3
+    nop                     # Delay slot
+
     addiu   $2,$2,1
     daddiu  $4,$4,1
     b       .L2
+    nop                     # Delay slot
 .L3:
     jr      $31
-    nop
+    nop                     # Delay slot
+main:
+    bal     strlen
+    nop                     # Delay slot
