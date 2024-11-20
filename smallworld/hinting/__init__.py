72fce5419a21f39bf15c268c6b8ebab25f511e45
andy <andy@bigandian.com>
2024-11-20 13:06:35 -0500

    linting

--- smallworld/hinting/__init__.py
+++ smallworld/hinting/__init__.py
@@ -1,8 +1,5 @@
-from .hinting import *  # noqa: F401, F403
 from .hinting import __all__ as __hinting__
-from .hints import *
 from .hints import __all__ as __hints__
-from .utils import *
 from .utils import __all__ as __utils__
 
 __all__ = __hinting__ + __hints__ + __utils__
