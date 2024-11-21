10a6b4e69fdc4ffcb9ce089806e836d11d08b7d5
andy <andy@bigandian.com>
2024-11-21 14:35:56 -0500

    isort

--- smallworld/hinting/__init__.py
+++ smallworld/hinting/__init__.py
@@ -1,9 +1,8 @@
 from .hinting import *  # noqa: F401, F403
-from .hints import *  # noqa: F401, F403
-from .utils import *  # noqa: F401, F403
-
 from .hinting import __all__ as __hinting__
+from .hints import *  # noqa: F401, F403
 from .hints import __all__ as __hints__
+from .utils import *  # noqa: F401, F403
 from .utils import __all__ as __utils__
 
 __all__ = __hinting__ + __hints__ + __utils__
