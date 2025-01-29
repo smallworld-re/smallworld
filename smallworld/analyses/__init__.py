1a8ecf5355feb25b99cda39c52feb88a0010ae1c
Alex Heinricher <aheinricher@gmail.com>
2025-01-29 11:21:52 -0500

    Updated so that tests pass.

--- smallworld/analyses/__init__.py
+++ smallworld/analyses/__init__.py
@@ -2,11 +2,10 @@ from .analysis import *  # noqa: F401, F403
 from .analysis import __all__ as __analysis__
 from .colorizer import Colorizer
 from .colorizer_summary import ColorizerSummary
-from .forced_exec import ForcedExecution
 
 # from .code_coverage import CodeCoverage
 # from .unstable import *  # noqa: F401, F403
 # from .unstable import __all__ as __unstable__
 
 # __all__ = __analysis__ + __unstable__ + ["CodeCoverage"]
-__all__ = __analysis__ + ["Colorizer", "ColorizerSummary", "ForcedExecution"]
+__all__ = __analysis__ + ["Colorizer", "ColorizerSummary"]
--- smallworld/analyses/forced_exec/forced_exec.py
+++ smallworld/analyses/forced_exec/forced_exec.py
@@ -1,16 +1,32 @@
 import logging
 import typing
 
-from ..emulators import AngrEmulator
-from ..exceptions import EmulationStop
-from ..platforms import Platform
-from ..state import Machine
-from .analysis import Analysis
+from ...emulators import AngrEmulator
+from ...exceptions import EmulationStop
+from ...platforms import Platform
+from ...state import Machine
+from ..analysis import Analysis
 
 log = logging.getLogger(__name__)
 
 
 class ForcedExecution(Analysis):
+    """Forced execution using angr
+
+    This allows you to emulate arbitrary program slices
+    by forcing the emulator to visit specific instructions,
+    ignoring the normal program control flow.
+
+    NOTE: This is not compatible with all architectures.
+    The architecture needs to support single-stepping;
+    delay slot architectures such as MIPS can't
+    be single-stepped by angr.
+
+    Arguments:
+        platform:   The platform you want to emulate
+        trace:      The list of program counter addresses you want to visit
+    """
+
     name = "forced-execution"
     description = "Forced execution using angr"
     version = "0.0.1"
@@ -26,7 +42,7 @@ class ForcedExecution(Analysis):
         emulator.initialize()
         try:
             for ip in self.trace:
-                emulator.write_register_content("eip", ip)
+                emulator.write_register_content("pc", ip)
                 emulator.state.registers.pp(log.info)
                 emulator.step()
         except EmulationStop:
