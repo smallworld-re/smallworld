4762462df162f79d914d04c024e3c2cc875c5a20
Alex Heinricher <aheinricher@gmail.com>
2025-03-03 16:06:33 -0500

    Example to show off input detection

---
+++ use_cases/input_detection/harness.py
@@ -0,0 +1,32 @@
+import logging
+import typing
+
+import smallworld
+
+# Set up logging and hinting
+smallworld.logging.setup_logging(level=logging.INFO)
+smallworld.hinting.setup_hinting(stream=True, verbose=True)
+
+# Create a machine
+machine = smallworld.state.Machine()
+
+
+# Load and add code into the state
+code = smallworld.state.memory.code.Executable.from_elf(
+    open("bin/test_input.amd64.elf", "rb"), address=0x10000000
+)
+machine.add(code)
+
+# Create a CPU
+cpu = smallworld.state.cpus.CPU.for_platform(code.platform)
+machine.add(cpu)
+
+# Set the instruction pointer to the target function
+pc = code.get_symbol_value("bazgorp")
+cpu.rip.set(pc)
+
+analyses: typing.List[
+    typing.Union[smallworld.analyses.Analysis, smallworld.analyses.Filter]
+] = [smallworld.analyses.InputDetection()]
+
+smallworld.analyze(machine)
---
+++ use_cases/input_detection/src/test_input.elf.c
@@ -0,0 +1,9 @@
+int foobar = 0;
+
+int bazgorp() {
+    return foobar;
+}
+
+int main() {
+    (void) bazgorp();
+}
