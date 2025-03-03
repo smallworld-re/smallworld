852caf754f84370d1b257f4ad92cbcbcc7ee13d0
Alex Heinricher <aheinricher@gmail.com>
2025-03-03 15:26:12 -0500

    Exported previously unexported analyses
    Before, we couldn't export these without breaing basic_harness.py.
    Making the analyze helper not run everything fixed that.

--- smallworld/analyses/__init__.py
+++ smallworld/analyses/__init__.py
@@ -2,10 +2,13 @@ from .analysis import *  # noqa: F401, F403
 from .analysis import __all__ as __analysis__
 from .colorizer import Colorizer
 from .colorizer_summary import ColorizerSummary
+from .field_detection import FieldDetectionAnalysis, ForcedFieldDetectionAnalysis
+from .forced_exec import ForcedExecution
 
-# from .code_coverage import CodeCoverage
-# from .unstable import *  # noqa: F401, F403
-# from .unstable import __all__ as __unstable__
-
-# __all__ = __analysis__ + __unstable__ + ["CodeCoverage"]
-__all__ = __analysis__ + ["Colorizer", "ColorizerSummary"]
+__all__ = __analysis__ + [
+    "Colorizer",
+    "ColorizerSummary",
+    "FieldDetectionAnalysis",
+    "ForcedFieldDetectionAnalysis",
+    "ForcedExecution",
+]
