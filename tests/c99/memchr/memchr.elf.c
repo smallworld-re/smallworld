32c63b2d02c8050a3a6074393ee1b3c8911f39a6
Alex Heinricher <aheinricher@gmail.com>
2025-07-01 16:17:59 -0400

    Memchr tests working

--- tests/c99/memchr/memchr.elf.c
+++ tests/c99/memchr/memchr.elf.c
@@ -1,5 +1,14 @@
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    return 0;
+    char *bad = (char *)(size_t)0xdead;
+    
+    char *data = NULL;
+    data = memchr(argv[1], 'o', 4);
+
+    if (data == NULL) {
+        return *bad;
+    }
+    exit(0);
 }
