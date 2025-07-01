b2973a700fa49e76d95864ee0aedd896259e8ac6
Alex Heinricher <aheinricher@gmail.com>
2025-07-01 16:47:37 -0400

    Got memcmp

--- tests/c99/memcmp/memcmp.elf.c
+++ tests/c99/memcmp/memcmp.elf.c
@@ -1,5 +1,11 @@
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    return 0;
+    char *bad = (char *)(size_t)0xdead;
+
+    if(!memcmp(argv[1], "foobar", 6)) {
+        exit(0);
+    }
+    return *bad;
 }
