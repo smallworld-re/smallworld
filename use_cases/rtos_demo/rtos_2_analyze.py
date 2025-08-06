f8200ba7cb79ee1034b7f4d204d70996913bef0c
wjanelle-ll <william.janelle@ll.mit.edu>
2025-08-06 13:13:53 -0400

    added fuzzing/dynamic analysis sections to rtos demo

---
+++ use_cases/rtos_demo/rtos_2_analyze.py
@@ -0,0 +1,83 @@
+import logging
+
+import smallworld
+
+# Types
+from smallworld.state.cpus.arm import ARM
+from smallworld.state.memory.stack.arm import ARM32Stack
+
+# Logging/hinting
+smallworld.logging.setup_logging(level=logging.INFO)
+smallworld.hinting.setup_hinting(stream=True, verbose=True)
+log = logging.getLogger("smallworld")
+
+###################
+# HARNESS CONFIGURATION
+
+# Machine
+machine = smallworld.state.Machine()
+
+# Code
+with open("./zephyr.elf", "rb") as f:
+    code = smallworld.state.memory.code.Executable.from_elf(f, page_size=1)
+    machine.add(code)
+
+# CPU
+cpu: ARM = smallworld.state.cpus.CPU.for_platform(code.platform)
+machine.add(cpu)
+
+# Emulator
+emulator = smallworld.emulators.AngrEmulator(code.platform)
+emulator.enable_linear()
+
+# Entry point / exit point
+entry_point = code.get_symbol_value("smallworld_bug")
+exit_point = 0x102368  # End of smallworld_bug, found via reverse engineering
+cpu.pc.set(entry_point)
+emulator.add_exit_point(exit_point)
+
+# Stack
+stack: ARM32Stack = smallworld.state.memory.stack.Stack.for_platform(
+    cpu.platform, 0x2000, 0x4000
+)
+machine.add(stack)
+sp = stack.get_pointer()
+cpu.sp.set(sp)
+
+# Input buffer
+buffer_memory_address = 0x1000
+# input_bytes = b"8x88888\xE18\x17\xF3\x1Faaa"
+# input_bytes = b"\x7F8888888=.3a"
+input_bytes = b"abcdefghijklmnop"
+buffer_memory = smallworld.state.memory.Memory(buffer_memory_address, len(input_bytes))
+buffer_memory[0] = smallworld.state.BytesValue(input_bytes, "input_buffer")
+machine.add(buffer_memory)
+
+# Pass the buffer to smallworld_bug
+cpu.r0.set(buffer_memory_address)
+cpu.r1.set(buffer_memory_address)
+cpu.lr.set_label("lr")
+
+###################
+# HARNESS EXECUTION
+machine.emulate(emulator)
+
+###################
+# DEBUGGING INFO
+
+# Investigate crashing instruction
+cpu.extract(emulator)
+# r6 = cpu.r6.get()
+# print(f"r6: {hex(r6)}")
+# r4 = cpu.r4.get()
+# print(f"r4: {hex(r4)}")
+# r8: claripy.ast.bv.BV = cpu.r8.get()
+# print(f"r8: {r8}")
+
+# Investigate symbolic stack memory
+stack_memory = emulator.read_memory_symbolic(0x2000, 0x4000)
+print(f"Symbolic stack memory: {stack_memory}")
+
+# Investigate memory above stack
+stack_memory = emulator.read_memory_symbolic(0x6000, 0x1000)
+print(f"After stack memory: {stack_memory}")
