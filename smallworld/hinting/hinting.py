b37671791c921de189dba3bbd92f7572210e9305
andy <andy@bigandian.com>
2025-08-14 17:24:25 -0400

    debug logging

--- smallworld/hinting/hinting.py
+++ smallworld/hinting/hinting.py
@@ -30,7 +30,7 @@ class Hinter:
         self.callbacks[clazz].append(callback)
 
     def send(self, hint: Hint) -> None:
-        logger.debug(json.dumps(hint))
+        logger.debug(json.dumps(hint.asdict()))
         clazz = hint.__class__
         if clazz in self.callbacks:
             for callback in self.callbacks[clazz]:
