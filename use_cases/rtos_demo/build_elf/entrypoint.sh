545b53bafbce3cd2c6984c254d4ebedc54a58fd9
wjanelle-ll <william.janelle@ll.mit.edu>
2025-08-26 16:06:06 -0400

    use zephyrprojectrtos/ci

--- use_cases/rtos_demo/build_elf/entrypoint.sh
+++ use_cases/rtos_demo/build_elf/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Initialize Zephyr workspace
+west init /zephyrproject
+cd /zephyrproject
+west update
+
+# Build patched binary
 patch /zephyrproject/zephyr/samples/net/sockets/echo_server/src/udp.c /opt/build_elf/udp.c.patch
 west build -b qemu_cortex_a9 zephyr/samples/net/sockets/echo_server
