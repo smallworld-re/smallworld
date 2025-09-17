15a10197a25b776389fdc1e94a567b1f7edf21a5
andy <andy@bigandian.com>
2025-09-17 15:47:48 -0400

    updated tests

--- tests/checked_heap/read/read.ppc.pcode.py
+++ tests/checked_heap/read/read.ppc.pcode.py
@@ -25,7 +25,9 @@ filename = (
     .replace(".pcode", "")
 )
 with open(filename, "rb") as f:
-    code = smallworld.state.memory.code.Executable.from_elf(f, platform=platform)
+    code = smallworld.state.memory.code.Executable.from_elf(
+        f, platform=platform
+    )
     machine.add(code)
 
 # Set the entrypoint to the address of "main"
@@ -71,7 +73,7 @@ code.update_symbol_value("free", free_model._address)
 emulator = smallworld.emulators.GhidraEmulator(platform)
 if isinstance(emulator, smallworld.emulators.AngrEmulator):
     emulator.enable_linear()
-
+    
 emulator.add_exit_point(entrypoint + 0x1000)
 try:
     machine.emulate(emulator)
