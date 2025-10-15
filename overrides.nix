5c19264b1a7aefe6c9f83ef6c687e3dfeb23e045
Rehosting Dev <rehostingdev@localhost>
2025-10-15 11:27:17 -0400

    set up uv and nix flake

---
+++ overrides.nix
@@ -0,0 +1,34 @@
+{ pkgs, python }:
+final: prev:
+let
+  buildSystemOverrides = {
+    bitarray.setuptools = [];
+    cppheaderparser.setuptools = [];
+    future.setuptools = [];
+    markupsafe.setuptools = [];
+    mulpyplexer.setuptools = [];
+    pypcode.setuptools = [];
+    pyyaml.setuptools = [];
+    timeout-decorator.setuptools = [];
+  };
+  mappedBuildSystemOverrides = builtins.mapAttrs (
+    name: spec:
+    prev.${name}.overrideAttrs (old: {
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ final.resolveBuildSystem spec;
+    })
+  ) buildSystemOverrides;
+in
+{
+  angr = prev.angr.overrideAttrs (old: {
+    autoPatchelfLibs = ["${python.pkgs.pyvex}/lib/python3.12/site-packages/pyvex/lib"];
+  });
+
+  cffi = prev.cffi.overrideAttrs (old: {
+    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [
+      pkgs.libffi
+    ] ++ (final.resolveBuildSystem {
+      setuptools = [];
+    });
+  });
+
+} // mappedBuildSystemOverrides
