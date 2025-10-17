a1257d60ad64bf72c6d4db943ecfad086459f0fb
Rehosting Dev <rehostingdev@localhost>
2025-10-17 14:42:42 -0400

    format unicornafl flake

--- unicornafl-flake/flake.nix
+++ unicornafl-flake/flake.nix
@@ -14,15 +14,22 @@
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in
     rec {
-        packages = forAllSystems (system: let
+      packages = forAllSystems (
+        system:
+        let
           pkgs = nixpkgs.legacyPackages.${system};
           pythonVersion = pkgs.python312;
-        in {
-          pythonEnv = pythonVersion.withPackages
-            (ps: [ (lib.${system}.pythonPackage ps) ] ++ (with ps; [ ipython ]));
-        });
+        in
+        {
+          pythonEnv = pythonVersion.withPackages (
+            ps: [ (lib.${system}.pythonPackage ps) ] ++ (with ps; [ ipython ])
+          );
+        }
+      );
 
-        lib = forAllSystems (system: let
+      lib = forAllSystems (
+        system:
+        let
           pkgs = nixpkgs.legacyPackages.${system};
           src = pkgs.fetchFromGitHub {
             owner = "AFLplusplus";
@@ -37,8 +44,10 @@
           cargoToml = builtins.fromTOML (builtins.readFile "${src}/Cargo.toml");
           projectName = cargoToml.package.name;
           projectVersion = cargoToml.package.version;
-        in {
-          pythonPackage = ps:
+        in
+        {
+          pythonPackage =
+            ps:
             ps.buildPythonPackage {
               pname = projectName;
               version = projectVersion;
@@ -62,6 +71,7 @@
                 pkgs.pkg-config
               ];
             };
-        });
+        }
+      );
     };
 }
