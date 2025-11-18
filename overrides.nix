ec9f6b7fafced42973a01edbc7e27dbdb75e1c47
Rehosting Dev <rehostingdev@localhost>
2025-11-18 11:22:43 -0500

    nix fmt overrides.nix

--- overrides.nix
+++ overrides.nix
@@ -2,37 +2,41 @@
 final: prev:
 let
   buildSystemOverrides = {
-    bitarray.setuptools = [];
-    cppheaderparser.setuptools = [];
-    future.setuptools = [];
-    markupsafe.setuptools = [];
-    mulpyplexer.setuptools = [];
-    pypcode.setuptools = [];
-    pyyaml.setuptools = [];
-    timeout-decorator.setuptools = [];
+    bitarray.setuptools = [ ];
+    cppheaderparser.setuptools = [ ];
+    future.setuptools = [ ];
+    markupsafe.setuptools = [ ];
+    mulpyplexer.setuptools = [ ];
+    pypcode.setuptools = [ ];
+    pyyaml.setuptools = [ ];
+    timeout-decorator.setuptools = [ ];
   };
   mappedBuildSystemOverrides = builtins.mapAttrs (
     name: spec:
     prev.${name}.overrideAttrs (old: {
-      nativeBuildInputs = (old.nativeBuildInputs or []) ++ final.resolveBuildSystem spec;
+      nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ final.resolveBuildSystem spec;
     })
   ) buildSystemOverrides;
 in
 {
   angr = prev.angr.overrideAttrs (old: {
-    autoPatchelfLibs = ["${python.pkgs.pyvex}/lib/python3.12/site-packages/pyvex/lib"];
+    autoPatchelfLibs = [ "${python.pkgs.pyvex}/lib/python3.12/site-packages/pyvex/lib" ];
   });
 
   cffi = prev.cffi.overrideAttrs (old: {
-    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [
-      pkgs.libffi
-    ] ++ (final.resolveBuildSystem {
-      setuptools = [];
-    });
+    nativeBuildInputs =
+      (old.nativeBuildInputs or [ ])
+      ++ [
+        pkgs.libffi
+      ]
+      ++ (final.resolveBuildSystem {
+        setuptools = [ ];
+      });
   });
 
   unicornafl = prev.unicornafl.overrideAttrs (old: {
-    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.unicorn];
+    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.unicorn ];
   });
 
-} // mappedBuildSystemOverrides
+}
+// mappedBuildSystemOverrides
